461482e7b838e1c40a39ea3c3e71e9df70427a44
SynTer <alpacanismo@gmail.com>
2021-04-30 18:36:39 -0300

    added resource manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games 'gta5'
+
+author 'SynTer#3819'
+description 'Resource to get all user info, like Inventory, cars in the garage, inventory items etc...'
+version '1.0'
+lua54 'yes'
+disable_lazy_natives 'yes'
+
+server_script 'svside.lua'
+
+server_only 'yes'
