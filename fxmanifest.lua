270907f4ef1f0c9add5f721d05594094b827466d
SynTer <alpacanismo@gmail.com>
2021-05-01 20:34:20 -0300

    getting player vehicles

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,6 @@
 -- Resource Metadata
 fx_version 'cerulean'
-games 'gta5'
+game 'gta5'
 
 author 'SynTer#3819'
 description 'Resource to get all user info, like Inventory, cars in the garage, inventory items etc...'
@@ -8,6 +8,9 @@ version '1.0'
 lua54 'yes'
 disable_lazy_natives 'yes'
 
-server_script 'svside.lua'
+server_scripts {
+    '@vrp/lib/utils.lua',
+    'svside.lua'
+}
 
 server_only 'yes'
